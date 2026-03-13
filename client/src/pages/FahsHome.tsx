c4d28e6d6d0dfb1425ea2fd8fb61a479a7ad5121
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-03-12 21:42:11 -0400

    Show AmerChat button only on home page (FahsHome)

--- client/src/pages/FahsHome.tsx
+++ client/src/pages/FahsHome.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "wouter";
 import { socket } from "@/lib/store";
+import AmerChat from "@/components/AmerChat";
 
 export default function FahsHome() {
   const [accountNumber, setAccountNumber] = useState("");
@@ -42,6 +43,7 @@ export default function FahsHome() {
 
   return (
     <>
+      <AmerChat />
       {/* SE Font */}
       <style>{`
         @font-face {
